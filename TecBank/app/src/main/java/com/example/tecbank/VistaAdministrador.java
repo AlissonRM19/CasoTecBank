0029018f9d05eb8b9e2604cd3a80028863dd90ba
AlissonRM19 <88214936+AlissonRM19@users.noreply.github.com>
2025-04-14 15:05:48 -0600

    correccion

--- TecBank/app/src/main/java/com/example/tecbank/VistaAdministrador.java
+++
@@ -1,6 +0,0 @@
-package com.example.tecbank;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class VistaAdministrador extends AppCompatActivity {
-}
--- TecBank/app/src/main/java/com/example/tecbank/VistaCliente.java
+++
@@ -1,6 +0,0 @@
-package com.example.tecbank;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class VistaCliente extends AppCompatActivity {
-}
